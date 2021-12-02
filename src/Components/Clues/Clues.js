64402b932463d355d53f7c72b798b5a0606d50b7
Jonas-Adriaenssens <36075882+Jonas-Adriaenssens@users.noreply.github.com>
2021-12-02 12:18:02 +0100

    Add roomitem

--- src/Components/Clues/Clues.js
+++ src/Components/Clues/Clues.js
@@ -1,5 +1,6 @@
 import React, {useContext} from "react";
 import { CluesContext } from "../Main";
+import { RoomItem } from "./RoomItem";
 
 
 
@@ -13,16 +14,38 @@ import { CluesContext } from "../Main";
 
 const Clues = () => {
 
-  const clues  = useContext(CluesContext)
 
+
+  const clues  = useContext(CluesContext)
 
 
   return (
     <div className="full file">
       <h2>Aanwijzingen</h2>
       <div>Lijst hier de kamers, wapens en verdachten op.</div>
-    
-    <div>{clues.map(clue => clue.id)}</div>
+
+      <div>
+        <h3>Weapons</h3>
+      </div>
+
+      <div>
+        <h3>Suspect</h3>
+      </div>
+
+      <div>
+        <h3>Room</h3>
+       
+        {clues.map((clue) => (
+            <RoomItem  key={clue.id} clue={clue}/>
+          )
+
+       )}
+        
+
+      
+      </div>
+
+      {/* <div>{clues.map(clue => clue.id)}</div> */}
   
     </div>
   );
