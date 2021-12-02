4f29bd065fb1eceebdfdb152dd6ba34e914ebdbc
Jonas-Adriaenssens <36075882+Jonas-Adriaenssens@users.noreply.github.com>
2021-12-02 13:33:05 +0100

    add filter to dropdown

--- src/Components/Forms/MakeSuggestionForm.js
+++ src/Components/Forms/MakeSuggestionForm.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, {useContext} from "react";
 import { useSettings } from "../context/useSettings";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import Select from 'react-select';
+import { CluesContext } from "../Main";
 
 /*
    SUGGESTIE FORMULIER
@@ -9,11 +13,32 @@ import { useSettings } from "../context/useSettings";
 
 export const MakeSuggestionForm = ({ gameKey, selectedRoom }) => {
   const { settings } = useSettings();
+  const clues  = useContext(CluesContext)
+
+  const filteredClues = (filterWord) => {
+    return clues.filter(clue => clue.type == filterWord)
+  }
 
   return (
     <div>
       <h2>Maak een suggestie</h2>
-      <p>Maak een formulier om een suggestie te maken.</p>
+
+    <h4>Weapon: </h4>
+    <select>
+        {filteredClues("weapon").map(clue =>
+           <option key={clue.id} value={clue.title}>{clue.title}</option>
+        )};
+    </select>
+
+    <h4>Suspect: </h4>
+    <select>
+        {filteredClues("suspect").map(clue =>
+           <option key={clue.id} value={clue.title}>{clue.title}</option>
+        )};
+    </select>
+
+          
+    
     </div>
   );
 };
